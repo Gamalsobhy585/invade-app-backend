2b61c99e66f05a873e4fad145d2aa9f2d5435871
Gamalsobhy585 <eng.gamalsobhi@gmail.com>
2025-04-06 22:12:35 +0200

    add laravel sanctum package

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,13 +3,14 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -20,7 +21,6 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
-        'access_token', 
     ];
 
     /**

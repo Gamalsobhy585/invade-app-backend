f30d803c0a4a875bf2b89fc02fc85d2b2d0c2244
Gamalsobhy585 <eng.gamalsobhi@gmail.com>
2025-04-07 02:02:19 +0200

    update appservice provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,18 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use app\Repositories\Interface\ITask;
+use app\Repositories\Implementation\TaskRepository;
+use app\Repositories\Interface\ICategory;
+use app\Repositories\Implementation\CategoryRepository;
+use app\Repositories\Implementation\UserRepository;
+use App\Repositories\Interface\IUser;
+use app\Services\Interface\IAuthService;
+use app\Services\Interface\ITaskService;
+use app\Services\Interface\ICategoryService;
+use app\Services\AuthService;
+use app\Services\TaskService;
+use app\Services\CategoryService;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,13 +23,20 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(ITask::class, TaskRepository::class);
+        $this->app->bind(ICategory::class, CategoryRepository::class);
+        $this->app->bind(IUser::class,UserRepository::class);
+        $this->app->bind(IAuthService::class,AuthService::class);
+        $this->app->bind(ITaskService::class,TaskService::class);
+        $this->app->bind(ICategoryService::class,CategoryService::class);
+    
+
     }
 
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    public function boot()
     {
         //
     }
